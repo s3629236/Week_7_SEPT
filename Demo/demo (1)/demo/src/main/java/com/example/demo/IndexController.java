ad2eb93de3facf4dce77b8c48c8f9e4583b452e8
Aidan Holland <s3629236@student.rmit.edu.au>
2019-09-12 17:24:19 +1000

    Pushing to github

---
+++ Demo/demo (1)/demo/src/main/java/com/example/demo/IndexController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+    @RequestMapping("/")
+    public class IndexController {
+
+        @RequestMapping(method = RequestMethod.GET)
+        public ModelAndView index() {
+            ModelAndView modelAndView = new ModelAndView("app/index");
+            return modelAndView;
+        }
+
+    }
+
